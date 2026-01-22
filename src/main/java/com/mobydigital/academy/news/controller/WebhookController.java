39d87ea77d72d328352f83f7c5218fdeac09bd90
Manuel Barcena <mbarcena@mobydigital.com>
2026-01-22 16:21:31 -0300

    Agregar la documentacion faltante de Swagger
    Se agrego la documentacion faltante de swagger en los controllers

--- src/main/java/com/mobydigital/academy/news/controller/WebhookController.java
+++ src/main/java/com/mobydigital/academy/news/controller/WebhookController.java
@@ -5,7 +5,10 @@ import com.mobydigital.academy.news.dto.NewsDto;
 import com.mobydigital.academy.news.service.ContentfulService;
 import com.mobydigital.academy.news.service.NotificationService;
 
-import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -14,31 +17,37 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 import java.util.Optional;
 
-@Hidden
 @RestController
 @RequestMapping("/contentful/webhook")
 @Slf4j
 @AllArgsConstructor
 public class WebhookController {
 
-    /*
-
-    Webhook -> te avisa q paso algo, observa si hay un cambio y tira un aviso
-    Websockets -> manda mensajes (tiempo real, se usa para chats o en este caso actualizar)
-
-     */
     private final ContentfulService contentfulService;
     private final NotificationService notificationService;
 
+    @Operation(summary = "Endpoint de prueba para enviar un mensaje a Kafka.",
+            description = "Este endpoint recibe un mensaje en el cuerpo de la solicitud y lo envía a un topic de Kafka.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Mensaje enviado exitosamente a Kafka."),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor.")
+    })
     @PostMapping("/kafka")
-    public ResponseEntity<String> mensajePrueba(@RequestBody String mensaje){
+    public ResponseEntity<String> mensajePrueba(@Parameter(description = "Mensaje de prueba para enviar a Kafka.") @RequestBody String mensaje){
         return new ResponseEntity<>(notificationService.sendKafka(mensaje), HttpStatus.OK);
     }
 
+    @Operation(summary = "Maneja los webhooks de Contentful.",
+            description = "Este endpoint recibe notificaciones de Contentful sobre cambios en el contenido, como publicaciones, despublicaciones y actualizaciones. Procesa estos eventos para invalidar la caché, notificar a otros servicios y mantener la consistencia de los datos.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Webhook procesado exitosamente."),
+            @ApiResponse(responseCode = "400", description = "Error en la solicitud del webhook, como un encabezado faltante."),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor durante el procesamiento del webhook.")
+    })
     @PostMapping
     public ResponseEntity<String> handleContentfulWebhook  (
-            @RequestHeader("X-Contentful-Topic") String topic,
-            @RequestBody(required = false) Map<String, Object> payload) throws RuntimeException, JsonProcessingException {
+            @Parameter(description = "El topic del webhook de Contentful, que indica el tipo de evento.") @RequestHeader("X-Contentful-Topic") String topic,
+            @Parameter(description = "El payload del webhook, que contiene los datos del evento.") @RequestBody(required = false) Map<String, Object> payload) throws RuntimeException, JsonProcessingException {
 
         log.info("Webhook recibido. Topic={}", topic);
         log.info("Payload={}", payload);
